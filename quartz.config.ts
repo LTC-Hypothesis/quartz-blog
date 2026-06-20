992c1253d1fc798ce541825e7d847ea74ee627ad
LaoLiu <HypothesisSZU@outlook.com>
2026-06-20 16:34:36 +0800

    auto sync 2026-06-20 16:34:36

--- quartz.config.ts
+++ quartz.config.ts
@@ -66,7 +66,6 @@ const config: QuartzConfig = {
         },
         keepBackground: false,
       }),
-      Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: false }),
       Plugin.GitHubFlavoredMarkdown(),
       Plugin.TableOfContents(),
       Plugin.CrawlLinks({ markdownLinkResolution: "shortest" }),
