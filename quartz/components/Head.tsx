35682e7cf27f2530d31c04b3fa511c5d243a2943
LaoLiu <HypothesisSZU@outlook.com>
2026-06-21 21:07:35 +0800

    添加 Google 验证 meta 标签

--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -40,6 +40,7 @@ export default (() => {
       <head>
         <title>{title}</title>
         <meta charSet="utf-8" />
+        <meta name="google-site-verification" content="4LkbxApWl4BDq9evfwUoBuWS0osEJXv7EUgZE9QkLAI" />
         {cfg.theme.cdnCaching && cfg.theme.fontOrigin === "googleFonts" && (
           <>
             <link rel="preconnect" href="https://fonts.googleapis.com" />
